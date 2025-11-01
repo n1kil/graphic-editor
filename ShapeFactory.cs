30a43c2a2139a9d9f53b56b31ba76cfd6d61a5d1
n1kil <nikchernyavsky@bk.ru>
2025-11-01 21:26:31 +1000

    add shape drawing classes

---
+++ ShapeFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphic_editor
+{
+    public class ShapeFactory
+    {
+        public Shape CreateShape(string shapeType, float x, float y, float width, float height)
+        {
+            switch (shapeType.ToLower())
+            {
+                case "rectangle":
+                    return new RectangleShape(x, y, width, height);
+                case "circle":
+                    return new Circle(x, y, width, height);
+                case "line":
+                    return new Line(x, y, x + width, y + height);
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ ShapeManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphic_editor
+{
+    public class ShapeManager
+    {
+        private List<Shape> shapes;
+        private Shape selectedShape;
+
+        public ShapeManager()
+        {
+            shapes = new List<Shape>();
+        }
+
+        public void AddShape(Shape shape)
+        {
+            shapes.Add(shape);
+        }
+
+        public void RemoveShape(Shape shape)
+        {
+            shapes.Remove(shape);
+        }
+
+        public Shape GetShapeAtPoint(float x, float y)
+        {
+            for (int i = shapes.Count - 1; i >= 0; i--)
+            {
+                if (shapes[i].ContainsPoint(x, y))
+                {
+                    return shapes[i];
+                }
+            }
+            return null;
+        }
+
+        public void DrawAllShapes(Graphics graphics)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw(graphics);
+            }
+        }
+
+        public void Clear()
+        {
+            shapes.Clear();
+            selectedShape = null;
+        }
+
+        public List<Shape> Shapes => shapes;
+        public Shape SelectedShape
+        {
+            get => selectedShape;
+            set
+            {
+                if (selectedShape != null)
+                    selectedShape.IsSelected = false;
+
+                selectedShape = value;
+
+                if (selectedShape != null)
+                    selectedShape.IsSelected = true;
+            }
+        }
+    }
+}
